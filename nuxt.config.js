ddf1e31660ca2794200b10f94549915604cef11d
Paul Em <paul3m@gmail.com>
2020-03-24 04:28:41 +0100

    add basic page setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,14 @@
 const pkg = require('./package');
 
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-  router: {
-    base: '/covid-19-curves/',
-  },
-} : {};
+const routerPath = process.env.DEPLOY_ENV === 'GH_PAGES' ? `/${pkg.name}/` : '/';
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES'
+  ? {
+    router: {
+      base: routerPath,
+    },
+  }
+  : {};
 
 
 module.exports = {
@@ -23,24 +26,24 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/covid-19-curves/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: `/${routerPath}/favicon.ico` },
       {
         rel: 'apple-touch-icon',
         type: 'image/png',
         sizes: '180x180',
-        href: '/covid-19-curves/apple-touch-icon.png',
+        href: `/${routerPath}/apple-touch-icon.png`,
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '32x32',
-        href: '/covid-19-curves/favicon-32x32.png',
+        href: `/${routerPath}/favicon-32x32.png`,
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '16x16',
-        href: '/covid-19-curves/favicon-16x16.png',
+        href: `/${routerPath}/favicon-16x16.png`,
       },
     ],
   },
